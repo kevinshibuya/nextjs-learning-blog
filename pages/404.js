bda4799a797175571bbae50d7c08eb6247194634
Kevin <kevin@flowautobody.com.au>
2023-10-10 20:35:51 -0300

    refining page not found component

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,17 @@
 import Head from "next/head";
+import Link from "next/link";
+import styles from "../components/layout.module.scss";
 
 export default function Custom404() {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Page not found!</title>
       </Head>
       <h1>404 - Page Not Found</h1>
-    </>
+      <div className={styles.backToHome}>
+        <Link href="/">← Back to home</Link>
+      </div>
+    </div>
   );
 }
