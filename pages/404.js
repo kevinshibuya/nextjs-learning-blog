f4f8392354ba140eebde4958d61b16216fec8395
Kevin <kevin@flowautobody.com.au>
2023-10-10 20:28:45 -0300

    custom page not found component with 404.js

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Custom404() {
+  return (
+    <>
+      <Head>
+        <title>Page not found!</title>
+      </Head>
+      <h1>404 - Page Not Found</h1>
+    </>
+  );
+}
